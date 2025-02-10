c33fe1b01197bd44936658d89a2264cbe52caacd
Maharavo Bab's <132753016+Maharavo1@users.noreply.github.com>
2025-02-10 15:44:08 +0300

    feat: add HelloController with /hello endpoint

---
+++ src/main/java/com/exercice/demo/endpoint/rest/controller/health/HelloController.java
@@ -0,0 +1,17 @@
+
+package com.exercice.demo.endpoint.rest.controller.health;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@AllArgsConstructor
+public class HelloController {
+    @GetMapping("/hello")
+  public String ping() {
+    return "hello-std22080";
+  }
+}
